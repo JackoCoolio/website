2c4bf8f504449dc7caa2a86d001782f38e6f0aee
Jackson Wambolt <jtwambolt@gmail.com>
2025-04-05 05:31:48 +0000

    create generic api for loading documents

--- src/pages/events.rs
+++ src/pages/events.rs
@@ -1,14 +1,15 @@
 use maud::{html, Markup, PreEscaped, Render};
 
-use crate::common::highlighted;
+use crate::{common::highlighted, pages::load_items_in_dir};
 
-use events::{load_events, Event};
-
-mod events;
+use super::FromDocument;
 
 pub fn content() -> Markup {
-    let mut events = load_events().unwrap_or([].into());
-    events.reverse();
+    let mut events: Vec<Event> = load_items_in_dir("public/events")
+        .into_iter()
+        .flatten()
+        .collect();
+    events.sort_by(|a, b| a.sort_key.cmp(&b.sort_key).reverse());
 
     html! {
         div #events {
@@ -57,6 +58,42 @@ impl Render for Event {
                     p { (PreEscaped(content)) }
                 }
             }
+        }
+    }
+}
+
+struct Event {
+    pub title: String,
+    pub start: String,
+    pub end: Option<String>,
+    pub content: String,
+    sort_key: String,
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct EventFrontMatter<'a> {
+    title: &'a str,
+    start: &'a str,
+    end: Option<&'a str>,
+    sort_key: Option<&'a str>,
+}
+
+impl FromDocument for Event {
+    type FrontMatter<'de> = EventFrontMatter<'de>;
+    fn from_parts<'c, 'de>(front_matter: Self::FrontMatter<'de>, content: &'c str) -> Self
+    where
+        Self: 'static,
+    {
+        Self {
+            title: front_matter.title.to_owned(),
+            start: front_matter.start.to_owned(),
+            end: front_matter.end.map(ToOwned::to_owned),
+            sort_key: front_matter
+                .sort_key
+                .unwrap_or(front_matter.start)
+                .to_owned(),
+            content: content.to_owned(),
         }
     }
 }
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,5 +1,9 @@
+use std::{io::Write, path::Path};
+
 use axum::Router;
+use comrak::{nodes::NodeValue, Arena};
 use maud::{html, Markup, Render};
+use serde::Deserialize;
 
 use crate::summary_pane;
 
@@ -61,4 +65,98 @@ fn page(meta: PageMetadata, inner: Markup) -> Markup {
             }
         }
     }
+}
+
+enum FromDocumentError {
+    ReadFailure,
+    WriteFailure,
+    InvalidFrontMatter,
+}
+
+const FRONT_MATTER_DELIMITER: &str = "---";
+
+trait FromDocument: Sized
+where
+    Self: 'static,
+{
+    type FrontMatter<'de>: Deserialize<'de>
+    where
+        Self: 'de;
+
+    fn from_parts<'c, 'de>(front_matter: Self::FrontMatter<'de>, content: &'c str) -> Self
+    where
+        Self: 'static;
+
+    fn from_document(path: impl AsRef<Path>) -> Result<Self, FromDocumentError> {
+        let document =
+            std::fs::read_to_string(path.as_ref()).map_err(|_| FromDocumentError::ReadFailure)?;
+
+        let arena = Arena::new();
+
+        let options = comrak::Options {
+            extension: comrak::ExtensionOptions::builder()
+                .front_matter_delimiter(FRONT_MATTER_DELIMITER.to_owned())
+                .build(),
+            ..Default::default()
+        };
+
+        let root = comrak::parse_document(&arena, &document, &options);
+
+        let front_matter_delim: Vec<_> = FRONT_MATTER_DELIMITER.chars().collect();
+
+        // get raw front-matter blob
+        let front_matter = root
+            .descendants()
+            .find_map(|node| match &node.data.borrow().value {
+                comrak::nodes::NodeValue::FrontMatter(x) => Some(x.clone()),
+                _ => None,
+            })
+            .ok_or(FromDocumentError::InvalidFrontMatter)?;
+        // trim '---' and newlines
+        let front_matter = front_matter
+            .trim()
+            .trim_matches(front_matter_delim.as_slice())
+            .trim();
+        // parse
+        let front_matter = serde_yaml::from_str(front_matter)
+            .map_err(|_| FromDocumentError::InvalidFrontMatter)?;
+
+        let content = {
+            let mut buf = Vec::new();
+            CustomFormatter::format_document(root, &options, &mut buf)
+                .map_err(|_| FromDocumentError::WriteFailure)?;
+            String::from_utf8_lossy(&buf).to_string()
+        };
+
+        Ok(Self::from_parts(front_matter, &content))
+    }
+}
+
+comrak::create_formatter!(CustomFormatter, {
+    NodeValue::Strong => |context, entering| {
+        if entering {
+            context.write_all(b"<span class=\"highlighted\"><span>")?;
+        } else {
+            context.write_all(b"</span><span class='highlight'></span><span class='shadow'></span></span>")?;
+        }
+    },
+});
+
+fn load_items_in_dir<D: FromDocument>(
+    dir_path: impl AsRef<Path>,
+) -> std::io::Result<impl Iterator<Item = D>> {
+    Ok(std::fs::read_dir(dir_path)?.flatten().filter_map(|entry| {
+        let file_type = entry.file_type().ok()?;
+        if !file_type.is_file() {
+            return None;
+        }
+
+        let file_name = entry.file_name(); // need separate let binding because returned OsStr is owned
+        let file_name = file_name.to_str()?;
+        if !file_name.ends_with(".md") {
+            return None;
+        }
+
+        D::from_document(entry.path()).ok()
+    }))
 }
